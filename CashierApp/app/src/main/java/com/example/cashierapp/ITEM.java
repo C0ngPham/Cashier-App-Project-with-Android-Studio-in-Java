d489b0c85b9c2170c40eebb1f32f24fd831e9580
congpham251299 <congpham251299@users.noreply.github.com>
2020-04-15 20:52:36 +0700

    Update

--- CashierApp/app/src/main/java/com/example/cashierapp/ITEM.java
+++ CashierApp/app/src/main/java/com/example/cashierapp/ITEM.java
@@ -2,12 +2,31 @@ package com.example.cashierapp;
 
 public class ITEM {
 
-    String item_name;
-    Integer item_price;
 
-    public ITEM(String item_name, Integer item_price) {
+    public String item_name;
+    public Integer item_price;
+    public String item_description;
+    public Integer item_img;
+    public ITEM(String item_name, Integer item_price, String item_description, Integer item_img) {
         this.item_name = item_name;
         this.item_price = item_price;
+        this.item_description = item_description;
+        this.item_img = item_img;
+    }
+
+    public String getItem_description() {
+        return item_description;
+    }
+
+    public void setItem_description(String item_description) {
+        this.item_description = item_description;
+    }
+    public Integer getItem_img() {
+        return item_img;
+    }
+
+    public void setItem_img(Integer item_img) {
+        this.item_img = item_img;
     }
 
     public String getItem_name() {
--- CashierApp/app/src/main/java/com/example/cashierapp/fragments/HomeFragment.java
+++ CashierApp/app/src/main/java/com/example/cashierapp/fragments/HomeFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.cashierapp.ITEM;
 import com.example.cashierapp.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +30,11 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     RecyclerView home_item_recyclerView;
+    MyAdapter adapter;
+    ArrayList <ITEM> data = new ArrayList<>();
+
+
+
 
 
     // TODO: Rename and change types of parameters
@@ -65,6 +77,71 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
+        home_item_recyclerView = v.findViewById(R.id.home_item_recyclerView);
+
+        genMockData();
+        adapter = new MyAdapter();
+
+        home_item_recyclerView.setAdapter(adapter);
+        home_item_recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //home_item_recyclerView.addItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+
         return v;
+    }
+
+    private void genMockData() {
+        data.add(new ITEM("Coffee",30000, "Black ice coffee no sugar", R.drawable.example));
+
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View itemView = layoutInflater.inflate(
+                    R.layout.home_recycler,
+                    parent,
+                    false
+            );
+            return new MyViewHolder(itemView);
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+            ITEM itemdata = data.get(position);
+            holder.item_name.setText(itemdata.getItem_name());
+            holder.item_price.setText(String.valueOf(itemdata.getItem_price()));
+
+            holder.item_description.setText(itemdata.getItem_description());
+
+            holder.item_img.setImageResource(itemdata.getItem_img());
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+
+    }
+    private class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView item_name;
+        TextView item_price;
+        TextView item_description;
+        ImageView item_img;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            item_name = itemView.findViewById(R.id.item_name);
+            item_price = itemView.findViewById(R.id.item_price);
+            item_description = itemView.findViewById(R.id.item_description);
+            item_img = itemView.findViewById(R.id.item_img);
+
+        }
+
     }
 }
